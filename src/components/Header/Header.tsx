5dfa3684878e49cf572906b9bdff42784dcbe1fc
Pail4 <73517684+Pail4@users.noreply.github.com>
2022-11-15 20:59:06 +0400

    eslint, mobx installed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { observer } from "mobx-react"
 import React from "react"
 
 @observer
-export class App extends React.Component {
+export class Footer extends React.Component {
     render() {
         return (
             <div></div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { App } from './App';
+import { Provider } from 'mobx-react'
+import { RootState } from './states';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    document.getElementById('root') as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const states = new RootState()
+
+root.render(
+    <Provider {...states}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Provider>
+);
